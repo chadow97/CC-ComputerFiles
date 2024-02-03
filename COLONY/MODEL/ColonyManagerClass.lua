304477c38cc1def44da86642ab2b0e1a9441a446
Emile Dallaire <dal1097@hotmail.com>
2024-02-03 12:23:39 -0500

    AUTO-COMMIT

--- COLONY/MODEL/ColonyManagerClass.lua
+++ COLONY/MODEL/ColonyManagerClass.lua
@@ -44,6 +44,7 @@ end
 
 function ColonyManagerClass:getColonyData()
     local status, colonyData = pcall(colIntUtil.getColony, self.colonyPeripheral)
+    logger.db({status, colonyData})
     if not status then
         colonyData = {}
     end
